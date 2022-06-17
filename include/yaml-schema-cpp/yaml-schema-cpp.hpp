afb87540592723f4c759883e821fd55e97a0549c
PepMS <jmarti@iri.upc.edu>
2022-06-17 08:12:40 +0200

    feat: add library. first draft

---
+++ include/yaml-schema-cpp/yaml-schema-cpp.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+#include <Eigen/Dense>
+
+#include "yaml-cpp/yaml.h"
+
+namespace yaml_schema_cpp
+{
+
+class SchemaValidator
+{
+  public:
+    SchemaValidator(const std::string& schema_path, const std::string& input_path, bool is_derived);
+    SchemaValidator(const std::string& schema_path, const YAML::Node& input_node, bool is_derived);
+    SchemaValidator(const std::string& schema_path, bool is_derived);
+
+    bool isValid(std::stringstream& log);
+
+    bool correctType(const YAML::Node& schema_node, const YAML::Node& input_node, std::stringstream& log);
+    void print();
+  private:
+    bool isValidDerived(std::stringstream& log);
+    bool isValidBase(std::stringstream& log);
+
+    void preProcess(YAML::Node& yaml_node);
+
+    bool is_derived_;
+
+    YAML::Node schema_node_;
+    YAML::Node input_node_;
+
+    std::string parent_schema_path_;
+    // std::string parent_input_path_;
+};
+
+}  // namespace yaml_schema_cpp
+
+namespace YAML
+{
+template <typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+struct convert<Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> >
+{
+    static Node encode(const Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& matrix)
+    {
+        Node node(NodeType::Sequence);
+
+        int nValues = matrix.rows() * matrix.cols();
+
+        for (int i = 0; i < nValues; ++i)
+        {
+            node.push_back(matrix(i / matrix.cols(), i % matrix.cols()));
+        }
+
+        return node;
+    }
+
+    static bool decode(const Node& node, Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& matrix)
+    {
+        // Totally Dynamic sized arrays are not supported
+        if (_Rows == Eigen::Dynamic && _Cols == Eigen::Dynamic)
+        {
+            // ROS_ERROR("Double Dynamic Matrices are not supported! Matrices may only be dynamic in one dimension!");
+            return false;
+        }
+
+        int nSize = node.size();  // Sequence check is implicit
+
+        // If one dimension is dynamic -> calculate and resize
+
+        // Rows Dynamic
+        if (_Rows == Eigen::Dynamic)
+        {
+            // Check
+            if (nSize % _Cols != 0)
+            {
+                // ROS_ERROR("Inputsize (%d) of dynamic row matrix is not a multiple of fixed column size (%d)", nSize,
+                // _Cols);
+                return false;
+            }
+
+            int nDynRows = nSize / _Cols;
+            matrix.resize(nDynRows, Eigen::NoChange);
+        }
+
+        // Cols Dynamic
+        if (_Cols == Eigen::Dynamic)
+        {
+            // Check
+            if (nSize % _Rows != 0)
+            {
+                // ROS_ERROR("Inputsize (%d) of dynamic column matrix is not a multiple of fixed row size (%d)", nSize,
+                // _Rows);
+                return false;
+            }
+
+            int nDynCols = nSize / _Rows;
+            matrix.resize(Eigen::NoChange, nDynCols);
+        }
+
+        if (nSize != matrix.rows() * matrix.cols())
+        {
+            // ROS_ERROR_STREAM("Unable to create Eigen::Matrix. Input is of wrong size! Matrix("
+            //                  << matrix.rows() << "x" << matrix.cols() << ") != Tokens(" << nSize << ")");
+            return false;
+        }
+        else
+        {
+            // fill
+            for (int i = 0; i < matrix.rows(); i++)
+            {
+                for (int j = 0; j < matrix.cols(); j++)
+                {
+                    // ROS_INFO_STREAM("Node pos: " << i*matrix.rows() + j << "Matrix: " << i << "," << j);
+                    //                                      YAML::Node field = node[i + j*matrix.cols()];
+                    //                                      matrix(i,j) = field.as<_Scalar>();
+
+                    matrix(i, j) = node[(int)(i * matrix.cols() + j)].as<_Scalar>();
+                }
+            }
+        }
+        return true;
+    }
+};
+}  // namespace YAML
---
+++ src/yaml-schema-cpp.cpp
@@ -0,0 +1,225 @@
+#include <filesystem>
+
+#include "yaml-schema-cpp/yaml-schema-cpp.hpp"
+
+namespace yaml_schema_cpp
+{
+
+SchemaValidator::SchemaValidator(const std::string& schema_path, const std::string& input_path, bool is_derived)
+    : is_derived_(is_derived)
+{
+    input_node_  = YAML::LoadFile(input_path);
+    schema_node_ = YAML::LoadFile(schema_path);
+
+    std::filesystem::path p_s(schema_path);
+    parent_schema_path_ = p_s.parent_path();
+
+    // SchemaValidator(schema_path, is_derived);
+}
+
+SchemaValidator::SchemaValidator(const std::string& schema_path, const YAML::Node& input_node, bool is_derived)
+    : is_derived_(is_derived)
+{
+    input_node_  = input_node;
+    schema_node_ = YAML::LoadFile(schema_path);
+
+    std::filesystem::path p_s(schema_path);
+    parent_schema_path_ = p_s.parent_path();
+}
+
+SchemaValidator::SchemaValidator(const std::string& schema_path, bool is_derived) : is_derived_(is_derived)
+{
+    // std::cout << "Constructor 2:" << schema_path << std::endl;
+
+    // std::filesystem::path p_i(input_path);
+
+    // parent_input_path_  = p_i.parent_path();
+}
+
+void SchemaValidator::print()
+{
+    std::cout << "Printing schema:" << std::endl;
+    std::cout << parent_schema_path_ << std::endl;
+    for (auto it = schema_node_.begin(); it != schema_node_.end(); ++it)
+    {
+        std::cout << it->first.as<std::string>() << std::endl;
+    }
+
+    std::cout << "Printing input:" << std::endl;
+    for (auto it = input_node_.begin(); it != input_node_.end(); ++it)
+    {
+        std::cout << it->first.as<std::string>() << std::endl;
+    }
+}
+
+bool SchemaValidator::isValid(std::stringstream& log)
+{
+    preProcess(schema_node_);  // follow substitution
+
+    log << "Log of YAML parsing procedure of file: \n";
+    log << "-------------------------------------- \n";
+
+    if (is_derived_)
+    {
+        return isValidDerived(log);
+    }
+
+    YAML::Node node_aux =
+        YAML::LoadFile("/home/pepms/robotics/libraries/yaml-schema-cpp/examples/yaml-schemas/stages/base.yaml");
+    // std::cout << parent_schema_path_ << std::endl;
+
+    return isValidBase(log);
+}
+
+bool SchemaValidator::isValidBase(std::stringstream& log)
+{
+    bool is_valid = true;
+
+    for (auto it = schema_node_.begin(); it != schema_node_.end(); ++it)
+    {
+        if (it->second["mandatory"].as<bool>())
+        {
+            // Check existence
+            const std::string field_name = it->first.as<std::string>();
+            if (YAML::Node i_node = input_node_[field_name])
+            {
+                const YAML::Node& sch_node = it->second;
+                if (!correctType(sch_node, i_node, log))
+                {
+                    log << "Field '" << it->first.as<std::string>() << "' of wrong type. Should be "
+                        << it->second["type"].as<std::string>() << std::endl;
+                    is_valid = false;
+                }
+            }
+            else
+            {
+                log << "Input yaml does not contain field: " << it->first.as<std::string>();
+                is_valid = false;
+            }
+        }
+        else
+        {
+            // If not mandatory it should have a default
+        }
+    }
+
+    return is_valid;
+}
+
+bool SchemaValidator::isValidDerived(std::stringstream& log)
+{
+    // Check the base case
+    bool is_valid = true;
+    is_valid      = is_valid && isValidBase(log);
+
+    // Check if the derived schema exists
+
+    std::string yaml_schema_path;
+    try
+    {
+        const std::string&          type = input_node_["type"].as<std::string>();
+        const std::filesystem::path derived_yaml(parent_schema_path_ + "/" + type + ".yaml");
+        if (!std::filesystem::exists(derived_yaml))
+        {
+            // The derived schema does not exists
+            std::string error = "Schema for derived type does not exists:" + derived_yaml.string();
+            throw std::runtime_error(error);
+        }
+        yaml_schema_path = derived_yaml.string();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    // Validate derived case
+    schema_node_ = YAML::LoadFile(yaml_schema_path);
+    preProcess(schema_node_);
+
+    return is_valid && isValidBase(log);
+}
+
+bool SchemaValidator::correctType(const YAML::Node& schema_node, const YAML::Node& input_node, std::stringstream& log)
+{
+    const std::string& type = schema_node["type"].as<std::string>();
+
+    if (type == "string")
+    {
+        try
+        {
+            input_node.as<std::string>();
+        }
+        catch (const std::exception& e)
+        {
+            return false;
+        }
+    }
+    else if (type == "double")
+    {
+        try
+        {
+            input_node.as<double>();
+        }
+        catch (const std::exception& e)
+        {
+            return false;
+        }
+    }
+    else if (type == "eigen_vector")
+    {
+        try
+        {
+            input_node.as<Eigen::VectorXd>();
+        }
+        catch (const std::exception& e)
+        {
+            return false;
+        }
+    }
+    else if (type == "sequence_own_type")
+    {
+        // First check that the node it is really a sequence
+        if (!input_node.IsSequence())
+        {
+            return false;
+        }
+
+        // Then check the validity of each element in the sequence
+        bool is_valid = true;
+        for (std::size_t i = 0; i < input_node.size(); i++)
+        {
+            std::string schema_path_own_type =
+                parent_schema_path_ + "/" + schema_node["own_type_location"].as<std::string>() + "/base.yaml";
+            SchemaValidator validator(schema_path_own_type, input_node[i], schema_node["polymorphism"].as<bool>());
+            is_valid = is_valid && validator.isValid(log);
+        }
+        return is_valid;
+    }
+
+    return true;
+}
+
+void SchemaValidator::preProcess(YAML::Node& yaml_node)
+{
+    // Substitute 'follow' in the schema node
+    // TODO: recursively
+    try
+    {
+        if (yaml_node["follow"])
+        {
+            std::string file_name = parent_schema_path_ + "/" + yaml_node["follow"].as<std::string>();
+            YAML::Node  node_base = YAML::LoadFile(file_name);
+            for (auto it = node_base.begin(); it != node_base.end(); ++it)
+            {
+                yaml_node.force_insert(it->first, it->second);
+            }
+            yaml_node.remove("follow");
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+}  // namespace yaml_schema_cpp
