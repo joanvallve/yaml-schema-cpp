39486406327387ee8825b3d4079fd931f80ac52e
cont-integration <CI@iri.upc.edu>
2022-07-28 08:57:25 +0000

    [skip ci] license headers added or modified

--- test/gtest_conversions.cpp
+++ test/gtest_conversions.cpp
@@ -1,3 +1,22 @@
+//--------LICENSE_START--------
+//
+// Copyright (C) 2022 Institut de Robòtica i Informàtica Industrial, CSIC-UPC.
+// Authors: Josep Martí Saumell (jmarti@iri.upc.edu) and Joan Vallvé Navarro (jvallve@iri.upc.edu)
+// All rights reserved.
+//
+// YAML-SCHEMA-CPP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.//
+//--------LICENSE_END--------
 #include "gtest/utils_gtest.h"
 #include "yaml-schema-cpp/yaml_conversion.hpp"
 #include <yaml-cpp/yaml.h>
